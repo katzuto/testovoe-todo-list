18a95b3dfd19e2daf2172d5152d5625e8cbbbf5e
katzuto <vstepovoi@gmail.com>
2024-08-20 14:21:13 +0300

    update files

--- todo-list-react/src/Components/Store/store.js
+++
@@ -1,9 +0,0 @@
-
-import { combineReducers, createStore } from "redux"
-import { taskReducer } from "./taskReducer"
-
-const rootReducer = combineReducers({
-    tasks: taskReducer
-})
-
-export const store = createStore(rootReducer)
